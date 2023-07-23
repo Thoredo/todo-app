784e3efb9c485b3cd7248854877814858a8661de
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-23 12:29:50 +0200

    remove "\n" in get_todo_list, add "\n" in write_todo_list

--- functions.py
+++ functions.py
@@ -7,11 +7,12 @@ def get_todo_list(filepath=FILEPATH):
     """
     with open(filepath, 'r') as file_local:
         todo_list_local = file_local.readlines()
+    todo_list_local = [todo.strip("\n") for todo in todo_list_local]
     return todo_list_local
 
 
 def write_todo_list(todos, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
+    todos = [arg + "\n" for arg in todos]
     with open(filepath, 'w') as file:
             file.writelines(todos)
-
