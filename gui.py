89b8f0316fb24b0fdca123949a1dbb110911d4cb
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-23 11:34:52 +0200

    implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -2,16 +2,22 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
+
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+
 list_box = sg.Listbox(values=functions.get_todo_list(), key="todo_list", 
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+
+exit_button =sg.Button("Exit")
 
 window = sg.Window('My To-Do App', 
                    layout=[[label], 
                            [input_box, add_button], 
-                           [list_box, edit_button]], 
+                           [list_box, edit_button, complete_button],
+                           [exit_button]], 
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -35,6 +41,15 @@ while True:
                 todo_list[index] = new_todo + "\n"
                 functions.write_todo_list(todo_list)
                 window['todo_list'].update(values=todo_list)
+        case "Complete":
+            todo_to_complete = values['todo_list'][0]
+            todo_list = functions.get_todo_list()
+            todo_list.remove(todo_to_complete)
+            functions.write_todo_list(todo_list)
+            window['todo_list'].update(values=todo_list)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case "todo_list":
             window['todo'].update(value=values['todo_list'][0])
         case sg.WIN_CLOSED:
